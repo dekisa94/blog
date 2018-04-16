7df49e0c04557974e80082f48253247a72c70f03
dekisa94 <dsimic94@gmail.com>
2018-04-16 19:47:48 +0200

    add seeds

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $values = ['Blogging', 'Freelancing', 'How to Succeed', 'Internet Marketing'];
+
+        App\Tag::truncate();
+
+        foreach($values as $value)
+        {
+            App\Tag::create([
+                'name' => $value
+            ]);
+        }
+
+        App\Post::all()->each(function (App\Post $post) use ($values){
+            $rndIds= App\Tag::inRandomOrder()->select('id')->take(3)->pluck('id');
+            $post->tags()->attach($rndIds);
+        });
+    }
+}
